0a205356d23d368c160186681e303d42970c551b
Sandip Sah <sandipsah1205@gmail.com>
2021-06-30 10:21:06 +0200

    add codes

--- backend/index.js
+++ backend/index.js
@@ -3,42 +3,30 @@ import mongodb from "mongodb";
 import dotenv from "dotenv";
 import RestaurantsDAO from "./dao/restaurantsDAO.js";
 import ReviewsDAO from "./dao/reviewsDAO.js";
-
 dotenv.config();
 const MongoClient = mongodb.MongoClient;
 
-const port = process.env.PORT;
-const url = process.env.RESTREVIEWS_DB_URI;
+const port = process.env.PORT || 8000;
 
-// const client = new MongoClient.connect(uri, {
-//   poolSize: 50,
-//   wtimeout: 2500,
-//   useNewUrlParse: true,
-//   useUnifiedTopology: true
-// })
-//   .catch((err) => {
-//     console.error(err.stack);
-//     //process.exit(1)
-//   })
-//   .then(async (client) => {
-//     await RestaurantsDAO.injectDB(client);
-//     await ReviewsDAO.injectDB(client);
-//     app.listen(port, () => {
-//       console.log(`listening on port ${port}`);
-//     });
-//   });
+MongoClient.connect(
+  process.env.REST_REVIEWS_DB_URI,
+  { native_parser: true },
+  function (err, db) {
+    assert.equal(null, err);
 
-// const client = new MongoClient(uri, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-// client.connect((err) => {
-//   await client.db(process.env.RESTREVIEWS_NS)
-//   .collection("restaurants");
-//   console.log("database connected")
-//   // perform actions on the collection object
-//   client.close();
-// });
+    db.collection(process.env.RESTREVIEWS_NS).update(
+      { a: 1 },
+      { b: 1 },
+      { upsert: true },
+      function (err, result) {
+        // assert.equal(null, err);
+        // assert.equal(1, result);
+        console.log(`connectio entering............`);
+        db.close();
+      }
+    );
+  }
+);
 
 // MongoClient.connect(
 //   process.env.RESTREVIEWS_DB_URI,
@@ -52,22 +40,10 @@ const url = process.env.RESTREVIEWS_DB_URI;
 //     //process.exit(1)
 //   })
 //   .then(async client => {
+//     console.log(`----------------${client}`)
 //     await RestaurantsDAO.injectDB(client)
 //     await ReviewsDAO.injectDB(client)
 //     app.listen(port, () => {
 //       console.log(`listening on port ${port}`)
 //     })
 //   })
-
-MongoClient.connect(url, function (err, client) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-
-  RestaurantsDAO.injectDB(client);
-  ReviewsDAO.injectDB(client);
-  app.listen(port, () => {
-    console.log(`listening on port ${port}`);
-  });
-
-  const db = client.db(dbName);
-});
--- frontend/src/components/add-reviews.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Addreviews() {
-  return <div>Hello world</div>;
-}
-
-export default Addreviews;
